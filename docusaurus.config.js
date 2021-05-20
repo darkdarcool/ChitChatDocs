088c7d979e8e9d08a47bcd5d4b1abb868bd6d4b3
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-05-20 10:18:16 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/Community', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/darkdarcool/ChitChat',
           label: 'GitHub',
@@ -78,13 +78,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/darkdarcool/ChitChatDocs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/darkdarcool/ChitChatDocs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
