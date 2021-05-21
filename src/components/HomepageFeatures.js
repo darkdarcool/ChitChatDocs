91f9ca9e837c900521358b90f2fec81745375536
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-05-21 13:22:31 -0700

    ha e

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,15 +5,33 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/logo.svg').default,
+    Svg: require('../../static/img/CodeLogo.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Secure, Strict types enforced, by default
       </>
     ),
   },
-  
+  {
+    title: 'Focus on What Matters',
+    Svg: require('../../static/img/logo.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by React',
+    Svg: require('../../static/img/secolog.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
 ];
 
 function Feature({Svg, title, description}) {
